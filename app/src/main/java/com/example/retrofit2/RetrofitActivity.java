efd488d7a2a9417be84f54151c096b3c88568e30
LacDuong <lacduongldg212@gmail.com>
2025-03-15 21:35:18 +0700

    Upload project Retrofit2

---
+++ app/src/main/java/com/example/retrofit2/RetrofitActivity.java
@@ -0,0 +1,80 @@
+package com.example.retrofit2;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RetrofitActivity extends AppCompatActivity {
+    RecyclerView rcCate;
+
+    CategoryAdapter categoryAdapter;
+    APIService apiService;
+    List<Category> categoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        AnhXa();
+        GetCategory();
+    }
+
+    private void AnhXa(){
+        rcCate = (RecyclerView) findViewById(R.id.rc_category);
+    }
+
+    private void GetCategory() {
+        // Gọi Instance và thực hiện luôn Request
+        apiService = RetrofitClient.getRetrofit().create(APIService.class);
+        apiService.getCategoryAll().enqueue(new Callback<List<Category>>() {
+            @Override
+            public void onResponse(Call<List<Category>> call, Response<List<Category>> response) {
+                if (response.isSuccessful()) {
+                    categoryList = response.body(); // Nhận dữ liệu hằng
+
+                    // Khởi tạo Adapter
+                    categoryAdapter = new CategoryAdapter(RetrofitActivity.this, categoryList);
+                    rcCate.setHasFixedSize(true);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(
+                            getApplicationContext(), LinearLayoutManager.HORIZONTAL, false
+                    );
+                    rcCate.setLayoutManager(layoutManager);
+                    rcCate.setAdapter(categoryAdapter);
+                    categoryAdapter.notifyDataSetChanged();
+                } else {
+                    int statusCode = response.code();
+                    // Handle request errors depending on status code
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+                Log.d("logg", t.getMessage());
+            }
+        });
+    }
+
+}
